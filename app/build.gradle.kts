774dfc4c98b10b56d8f01a8b384a8a736bfae5de
denningbn <denningbn@appstate.edu>
2023-09-28 14:28:19 -0400

    added room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 
 dependencies {
 
+    implementation("androidx.room:room-runtime:2.5.2")
+    annotationProcessor("androidx.room:room-compiler:2.5.2")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
